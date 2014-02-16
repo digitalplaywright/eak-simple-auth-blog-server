85044035b33fdbfaca3e97ddca75d287f1ab8c85
kagemusha <mmadrid99@gmail.com>
2014-02-16 10:58:05 -0500

    doorkeeper

--- Gemfile
+++ Gemfile
@@ -4,6 +4,7 @@ gem 'bcrypt-ruby', '~> 3.0.0'
 
 gem 'rails', '4.0.2'
 #gem 'cancan'
+gem 'doorkeeper'
 gem 'devise'
 gem 'pg'
 gem 'active_model_serializers'
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 EmAuthServer::Application.routes.draw do
+
+  use_doorkeeper
+
   root :to => "home#index"
+
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
 end
