274f2af547722fb48c096ad99ce321620e5f9e8b
Andreas Sæbjørnsen <andreas@digitalplaywright.com>
2014-04-24 20:05:34 -0700

    added token return current user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :null_session
 
+  # Find the user that owns the access token
+  def current_resource_owner
+    User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+  end
+
   private
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,12 +1,27 @@
 class UsersController < ApplicationController
-  before_filter :authenticate_user!
+  doorkeeper_for :all
 
   def index
+    unless params[:token].nil?
+      @user = current_resource_owner
+      @user.token = doorkeeper_token.token
+    end
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
+    current_id = params[:id]
+
+
+    if current_id == 'current'
+      @user = current_resource_owner
+      @user.token = doorkeeper_token.token
+    else
+      @user = User.find(params[:id])
+    end
+
+    render :json => @user
+
   end
 
 end
